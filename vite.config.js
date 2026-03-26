e303f7f1685ff9ce820e005197824f1f7105d8a4
Zico Permadi <105289253+zicoprmd@users.noreply.github.com>
2026-03-26 20:14:25 +0700

    Update vite.config.js add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ['klinik.zicozema.my.id', 'localhost', '127.0.0.1'],
